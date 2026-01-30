872a066111986f87d4cd152aab07d07f154e86cb
rian38951 <rian38951@cccneb.edu>
2026-01-29 22:03:34 -0600

    first update

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>
+        Rian Gorve – This is my first React Native app!
+      </Text>
+    </View>
+  );
+}
